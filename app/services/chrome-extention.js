17a1dea67e8ce39a225d9d4832a96e9e2955a558
Kirill Yakovenko <kirill.yakovenko@me.com>
2017-02-23 11:58:11 +0200

    Fixed

--- app/services/chrome-extention.js
+++ app/services/chrome-extention.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
     getCurrentTab() {
-        var promise = new Promise(function(resolve,reject){
+        return new Promise(function(resolve,reject){
           chrome.tabs.query({
             active: true,
             currentWindow: true
-        }, resolve);
-        });
-        return promise.then(function(value) {
-            return value[0];
-        });
+          }, v => resolve(v));
+        }).then(v => v[0]);
     }
 });
